81a38fb561ce78b3df6e4437e0a01fbdad5be3c0
misterdan100 <danielmca60@gmail.com>
2024-03-14 17:37:33 -0500

    Request to db using getServerSideProps in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+export default function Home({categorias}) {
+
+  console.log(categorias)
   return (
     <h1>Hola MisterDAN</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+
+  const categorias = await prisma.categoria.findMany({
+    where: {
+      nombre: 'Pizzas'
+    }
+  })
+  console.log(categorias)
+  return {
+    props: {categorias}
+  }
 }
