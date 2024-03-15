1529e9fcb5cca5263915085d63ef8ccc703f736c
misterdan100 <danielmca60@gmail.com>
2024-03-15 18:15:34 -0500

    Get categorias and its products, show products on main throught the product component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { PrismaClient } from '@prisma/client'
 import Layout from '../layout/Layout'
 import useQuiosco from '../hooks/useQuiosco'
+import Producto from '../components/Producto'
 
 export default function Home() {
   const { categoriaActual } = useQuiosco()
@@ -12,10 +13,19 @@ export default function Home() {
     <Layout
       pagina={`Menu ${categoriaActual?.nombre}`}
     >
-      <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
+      <h1 className="text-4xl font-black uppercase">{categoriaActual?.nombre}</h1>
       <p className='text-2xl my-10'>
         Elige y personaliza tu pedido a continuacion
       </p>
+
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+      {categoriaActual?.productos?.map( producto => (
+        <Producto 
+          key={producto.id}
+          producto={producto}
+        />
+      ))}
+      </div>
 
     </Layout>
   )
